663ad8bc0bffb6f7f217b28359759a71ce511c71
NurulIslam96 <nurul.islam96@aol.com>
2026-08-09 00:19:43 +0600

    first commit

---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# -------SQLITE3-------
+SQLAlCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+engine = create_engine(SQLAlCHEMY_DATABASE_URL)
+
+# -------POSTGRESQL-------
+# SQLAlCHEMY_DATABASE_URL = (
+#     "postgresql://postgres:password@localhost/TodoApplicationDatabase"
+# )
+# engine = create_engine(SQLAlCHEMY_DATABASE_URL)
+
+# -------MYSQL-------
+# SQLAlCHEMY_DATABASE_URL = (
+#     "mysql+pymysql://root:password@127.0.0.1:3306/todoapplicationdatabase"
+# )
+# engine = create_engine(SQLAlCHEMY_DATABASE_URL)
+
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
